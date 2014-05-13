b7455936fea3077605af32e6ba0e32ea10a8dbe7
nithn m <nithn@nithn-K52F.(none)>
2014-05-13 15:31:48 +0530

    changed var name

--- Project1.js
+++ Project1.js
@@ -1,12 +1,12 @@
 function change_colour(id)
 {
-	var colors=[
+	var change=[
 		"red",
 		"green",
 		"blue",
 		"white",
 		"yellow",
 		"black"
-	]
-	document.getElementById(id).style.backgroundColor=colors[Math.floor((Math.random() * 10)%7)];
+	];
+	document.getElementById(id).style.backgroundColor= change[Math.floor((Math.random() * 10)%7)];
 }
